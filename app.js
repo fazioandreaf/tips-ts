183c5dbdbd6e4a42f0369e15a4211a7dcd607e36
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-07-19 16:44:31 +0200

    sottotipi di void

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+userInput = 5;
+userInput = 'max';
+var userName;
+// userName=userInput;
+// si può usare come check
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-// void sta che può ritornare qualsiasi tipo, una funzione non può essere undefined
-function printResult(num) {
-    console.log('Result: ' + num);
+function generateError(message, code) {
+    throw {
+        message: message,
+        errorCode: code
+    };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-// accetto qualsiasi funzione che ha come paramentri di inserimento due numeri
-var combineValues;
-combineValues = add;
-// combineValues=printResult;
-// combineValues=5;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+generateError('An error', 500);
+console.log('ciao');
--- app.ts
+++ app.ts
@@ -2,5 +2,17 @@ let userInput:unknown;
 userInput=5;
 userInput='max';
 let userName:string;
-userName=userInput;
+// userName=userInput;
 // si può usare come check
+if(typeof userInput==='string'){
+    userName=userInput
+}
+// questa funzione non fa continuare il codice, non ritorna nulla mai
+function generateError(message:string,code:number) {
+    throw {
+        message:message,
+        errorCode:code
+    };
+}
+generateError('An error',500);
+console.log('ciao')
